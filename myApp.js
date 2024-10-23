dbe2f6044f978be553063470101176b9d2e1b288
xairban <157282919+xairban@users.noreply.github.com>
2024-10-23 22:38:28 +0800

    xairban

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.use(helmet.ieNoOpen());
 const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force:true}));
 
-
+app.use(helmet.dnsPrefetchControl());
 
 
 
