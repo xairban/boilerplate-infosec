1eb40a966ae85511891289f228b561ce0089d7f7
xairban <157282919+xairban@users.noreply.github.com>
2024-10-23 22:32:29 +0800

    xairban

--- myApp.js
+++ myApp.js
@@ -8,8 +8,9 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force:true}));
 const ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force:true}));
+
 
 
 
