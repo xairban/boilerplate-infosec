3744c96a9f1c7ea49e4bc90b4c4e88f16d7c800a
xairban <157282919+xairban@users.noreply.github.com>
2024-10-23 23:28:26 +0800

    xairban

--- myApp.js
+++ myApp.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-
 const ninetyDaysInSeconds = 90*24*60*60;
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force:true}));
 
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
+app.use(helmet({
+	hidePoweredBy(),
+	frameguard({ action: 'deny' }),
+	xssFilter(),
+	noSniff(),
+	ieNoOpen(),
+	hsts({maxAge: nenetyDaysInSeconds, force:true}),
+	dnsPrefetchControl(),
+	noCache(),
+	contentSecurityPolicy({
+		directives: {
+		default: ["'self'"],
+		scriptSrc: ["'self'", 'trusted-cdn.com'],
+		},
+	}),
+}))
 
-app.use(helmet.contentSecurityPolicy({
-	directives: {
-	defaultSrc:["'self'"],
-	scriptSrc:["'self'", 'trusted-cdn.com'],
-	}
-})
-)
 
 
 
